805636e03f1bde5e1738347a3318ef8913e331e9
davidmathias <thestardavid@gmail.com>
2025-12-17 15:24:41 +0530

    Model evaluator

--- src/main/java/com/david/agent/CognitoScoreAgent.java
+++ src/main/java/com/david/agent/CognitoScoreAgent.java
@@ -1,16 +1,18 @@
 package com.david.agent;
 
+import com.david.agent.models.Models;
 import com.embabel.agent.api.annotation.AchievesGoal;
 import com.embabel.agent.api.annotation.Action;
 import com.embabel.agent.api.annotation.Agent;
 import com.embabel.agent.api.annotation.Export;
 import com.embabel.agent.api.common.Ai;
-import com.embabel.agent.api.models.AnthropicModels;
 import com.embabel.agent.api.models.DeepSeekModels;
+import com.embabel.agent.domain.io.UserInput;
 import com.embabel.agent.domain.library.HasContent;
 import com.embabel.common.ai.model.LlmOptions;
 import com.embabel.common.core.types.Timestamped;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Profile;
 
@@ -23,6 +25,8 @@ import java.util.Map;
 @Profile("!test")
 public class CognitoScoreAgent {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     CognitoScoreAgent() {
     }
 
@@ -31,19 +35,30 @@ public class CognitoScoreAgent {
             export = @Export(remote = true, name = "LLM Evaluator"))
 
     @Action
-    public EvaluationResult evaluateLLM(Ai ai, LLMCandidates llmCandidates, TestKit testKit) {
+    public FinalResult evaluateLLM(UserInput userInput, Ai ai, TestKit testKit) {
+
+        com.david.agent.models.Models models = null;
+        try {
+            models = objectMapper.readValue(userInput.getContent(), Models.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (models == null || models.models().isEmpty()) {
+            throw new RuntimeException("No models provided");
+        }
         List<ExamResponse> responses = new ArrayList<>();
-        for (String alias : llmCandidates.llmNames().keySet()) {
+        for (String alias : models.models().keySet()) {
             for (Question question : testKit.questions()) {
                 String response = ai
-                        .withLlm(llmCandidates.llmNames.get(alias))
+                        .withLlm(models.models().get(alias))
                         .withPromptContributor(Personas.EXAMINER)
                         .createObject(question.text(), String.class);
                 ExamResponse examResponse = new ExamResponse(question.text(), response, alias);
                 responses.add(examResponse);
             }
         }
-        return ai.withLlm(DeepSeekModels.DEEPSEEK_CHAT).withPromptContributor(Personas.EVALUATOR).createObject(responses.toString(), EvaluationResult.class);
+        EvaluationResult evaluationResult = ai.withLlm(DeepSeekModels.DEEPSEEK_CHAT).withPromptContributor(Personas.EVALUATOR).createObject(responses.toString(), EvaluationResult.class);
+        return new FinalResult(evaluationResult, responses);
     }
 
     @Action
@@ -61,11 +76,6 @@ public class CognitoScoreAgent {
                 ).trim(), TestKit.class);
     }
 
-    @Action
-    LLMCandidates candidates() {
-        Map<String, String> llmNames = Map.of("A",AnthropicModels.CLAUDE_35_HAIKU,"B",AnthropicModels.CLAUDE_37_SONNET);
-        return new LLMCandidates(llmNames);
-    }
 
     public record Question(
             @JsonPropertyDescription("Question text")
@@ -101,20 +111,22 @@ public class CognitoScoreAgent {
     ) implements HasContent {
         @Override
         public @NotNull String getContent() {
-            return "Question asked to "+modelName+": "+question+"\n"+"Response from LLM: "+response;
+            return "Question asked to " + modelName + ": " + question + "\n" + "Response from LLM: " + response;
         }
     }
 
-    public record NameAndScore(
+    public record NameScoreAndRank(
             @JsonPropertyDescription("Name of the LLM")
             String name,
             @JsonPropertyDescription("Total Score of the LLM")
             int score,
             @JsonPropertyDescription("Rank of the LLM")
             int rank
-    ){}
+    ) {
+    }
+
     public record EvaluationResult(
-            List<NameAndScore> examResult
+            List<NameScoreAndRank> examResult
     ) implements HasContent, Timestamped {
         @Override
         public @NotNull String getContent() {
@@ -129,7 +141,10 @@ public class CognitoScoreAgent {
 
     public record LLMCandidates(
             @JsonPropertyDescription("List of LLM candidates")
-            Map<String,String> llmNames
+            Map<String, String> llmNames
     ) {
+    }
+
+    public record FinalResult(EvaluationResult evaluationResult, List<ExamResponse> examResponses) {
     }
 }
--- src/main/java/com/david/agent/controller/CognitoScoreController.java
+++ src/main/java/com/david/agent/controller/CognitoScoreController.java
@@ -1,23 +1,29 @@
 package com.david.agent.controller;
 
 import com.david.agent.CognitoScoreAgent;
+import com.david.agent.models.Models;
 import com.embabel.agent.api.common.autonomy.AgentInvocation;
 import com.embabel.agent.core.AgentPlatform;
 import com.embabel.agent.domain.io.UserInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CognitoScoreController {
 
     @Autowired
     private AgentPlatform agentPlatform;
+
+    @Autowired
+    ObjectMapper objectMapper;
     @PostMapping("/api/v1/run")
-    public String runAgent(@RequestBody String input){
-        CognitoScoreAgent.EvaluationResult output = AgentInvocation.create(agentPlatform, CognitoScoreAgent.EvaluationResult.class).invoke(new UserInput(input));
-        return output.getContent();
+    public CognitoScoreAgent.FinalResult runAgent(@RequestBody Models models) throws Exception{
+        String value = objectMapper.writeValueAsString(models);
+        return AgentInvocation.create(agentPlatform, CognitoScoreAgent.FinalResult.class).invoke(new UserInput(value));
     }
 }
